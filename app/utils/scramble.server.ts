7e83ccbcbb3c8f76f9071ed65251b7aed3036a51
Jack Boberg <hello@jackboberg.info>
2022-03-02 18:58:47 +0100

    WIP: use cubing to generate scramble

--- app/utils/scramble.server.ts
+++ app/utils/scramble.server.ts
@@ -1,5 +1,11 @@
+// import { randomScrambleForEvent } from 'cubing/scramble'
+import { split } from 'lodash'
+
 export type Alg = String[]
 
 export async function scramble (): Promise<Alg> {
-  return ['R', 'L', 'U', 'D', 'B', 'F']
+  // const s = await randomScrambleForEvent('333')
+  const s = 'R L U D B F'
+
+  return split(s.toString(), ' ')
 }
